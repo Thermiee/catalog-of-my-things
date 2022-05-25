88d6cea3d24b74be30b66f9ddada9938e381d412
Thermiee <navyblack16@yahoo.com>
2022-05-25 15:05:29 +0100

    fix linter errors

--- author.rb
+++ author.rb
@@ -1,11 +1,10 @@
 class Author
-    attr_accessor :first_name , :last_name , :id , :items
+  attr_accessor :first_name, :last_name, :id, :items
 
-    def initialize(first_name , last_name , id = Random.rand(1..1000))
-        @first_name = first_name
-        @last_name = last_name
-        @id = id
-        @items = []
-    end
-    
+  def initialize(first_name, last_name, id = Random.rand(1..1000))
+    @first_name = first_name
+    @last_name = last_name
+    @id = id
+    @items = []
+  end
 end
