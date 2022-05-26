02411c8cc90c70d5911c75f89df68ad0e0ad5e5a
Thermiee <navyblack16@yahoo.com>
2022-05-26 15:52:27 +0100

    add game and author unit test

--- app/game.rb
+++ app/game.rb
@@ -1,16 +1,17 @@
 class Game
-  attr_accessor :multiplayer, :last_played
+  attr_accessor :name, :published, :multiplayer, :last_played, :archived
 
-  def initialize(name, published, multiplayer, last_played)
+  def initialize(name, published, multiplayer, last_played, archived: false)
     @name = name
     @published = published
     @multiplayer = multiplayer
     @last_played = last_played
+    @archived = archived
   end
 
   private
 
   def can_be_archived?
-    super && (Time.now - @last_played) > 60
+    super && (last_played.year < Time.new.year - 2)
   end
 end
---
+++ spec/game_spec.rb
@@ -0,0 +1,37 @@
+require './app/game'
+require './app/author'
+require './app/item'
+
+describe Game do
+  context 'when a game is created' do
+    it 'should have a name' do
+      game = Game.new('Super Mario Bros', true, true, Time.now)
+
+      expect(game.name).to eq('Super Mario Bros')
+    end
+
+    it 'should have a published status' do
+      game = Game.new('Super Mario Bros', true, true, Time.now)
+
+      expect(game.published).to eq(true)
+    end
+
+    it 'should have a multiplayer status' do
+      game = Game.new('Super Mario Bros', true, true, Time.now)
+
+      expect(game.multiplayer).to eq(true)
+    end
+
+    it 'should have a last played date' do
+      game = Game.new('Super Mario Bros', true, true, Time.now)
+
+      expect(game.last_played).to be_a(Time)
+    end
+
+    it 'should have an archived status' do
+      game = Game.new('Super Mario Bros', true, true, Time.now)
+
+      expect(game.archived).to eq(false)
+    end
+  end
+end
