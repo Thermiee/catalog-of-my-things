91b5da38d956ceef7315e4d7b17fc228cc8affb1
Thermiee <navyblack16@yahoo.com>
2022-05-27 01:07:29 +0100

    add game and author json file

---
+++ data/author_crud.rb
@@ -0,0 +1,27 @@
+require 'json'
+
+def fetch_data(file)
+  file_path = "./data/#{file}.json"
+  File.new(file_path, 'w+') unless File.exist?(file_path)
+  File.write(file_path, '[]') if File.empty?(file_path)
+  contents = File.read(file_path)
+  JSON.parse(contents)
+end
+
+def update_data(file, data)
+  opts = {
+    array_nl: "\n",
+    object_nl: "\n",
+    indent: '  ',
+    space_before: ' ',
+    space: ' '
+  }
+  json_data = JSON.generate(data, opts)
+  File.write("./data/#{file}.json", json_data)
+end
+
+def authors
+  fetch_data('authors').map do |author|
+    Author.new(author['first_name'], author['last_name'], author['id'])
+  end
+end
