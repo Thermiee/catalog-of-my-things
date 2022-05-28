98276754b8f268cedd2e3a03518daf2b52c16c38
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-05-28 11:55:16 +0200

    Modified `author.rb`

--- game-and-author/author.rb
+++ game-and-author/author.rb
@@ -1,15 +1,29 @@
-require_relative '../app/item'
+# require 'json'
+require_relative '../app/helper'
 
-class Author < Item
-  attr_accessor :id, :first_name, :last_name, :items
+class Author
+  attr_accessor :id, :first_name, :last_name
+  attr_reader :items
 
-  def initialize(id, first_name, last_name, items)
-    super
+  include Help
+
+  def to_json(*_args)
+    JSON.generate({ id: @id, first_name: @first_name, last_name: @last_name })
+  end
+
+  def self.from_json(json)
+    Author.new(json['first_name'], json['last_name'], json['id'])
+  end
+
+  def initialize(first_name, last_name, id = rand(1000))
     @id = id
     @first_name = first_name
     @last_name = last_name
-    @items = items
+    @items = []
   end
 
-  def add_item(item)end
+  def add_item(item)
+    @items << item
+    item.add_author self
+  end
 end
